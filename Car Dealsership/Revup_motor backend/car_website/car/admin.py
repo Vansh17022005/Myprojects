739a2f68162971bf35775b959ae872f7b532c5e3
Vansh Raval <160266509+886616@users.noreply.github.com>
2024-10-22 23:11:11 +0530

    Agriculture

--- Car Dealsership/Revup_motor backend/car_website/car/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CarModel,Dealer,Appointment,UserProfile
-# Register your models here.
-admin.site.register(CarModel)
-admin.site.register(Dealer)
-admin.site.register(Appointment)
-admin.site.register(UserProfile)
